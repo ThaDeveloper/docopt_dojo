3646b1ba4b374097f88f01b3a5a384e5b0a6d952
Justin Ndwiga <ndwigajustin@gmail.com>
2017-10-29 15:03:22 +0300

    [Feature] add_person method on Person class

--- src/dojo.py
+++ src/dojo.py
@@ -2,12 +2,39 @@ class Dojo:
     pass
 
 class Person(Dojo):
-    pass
+    def __init__(self,name):
+        #Dojo.__init__(self,allocations,all_persons)
+        self.name = name
+        
+    def add_person(self,role,wants_accomodation="N"):
+        self.role = role
+        self.wants_accomodation = wants_accomodation
+        office = []
+        living = []
+        all_persons = []
+        if self.role == "Staff":
+            wants_accomodation = "N"
+            office.append(self.name)
+            all_persons.append(self.name)
+        all_persons.append(self.name)
+        office.append(self.name)
+        living.append(self.name)
+        if self.role == "Staff" and self.wants_accomodation == "Y":
+            return "A staff cannot be allocated living space"
+        if self.wants_accomodation == "Y":
+            return "The {} {} has been created and allocated office {} and living space {}".format(self.role,self.name,office[0],living[0])
+        return "The {} {} has been created and allocated office {}".format(self.role,self.name,office[0])
+       
 
 class Fellow(Person):
-    pass
+    """Inherits from Person"""
+    def __init__(self, name):
+        super().__init__(name, role="Fellow")
+	
 class Staff(Person):
-    pass
+    """Inherits from Person"""
+    def __init__(self, name):
+        super().__init__(name, role="Staff")
 
 class Room(Dojo):
     pass
@@ -15,3 +42,5 @@ class Office(Room):
     pass
 class LivingSpace(Room):
     pass
+j = Person("justin ndwiga")
+print(j.add_person("Fellow","N"))
